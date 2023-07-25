b1db45d2adc4a549fdefea2e4da9179045be48cb
MajharolTanvir <majharul5456@gmail.com>
2023-07-25 21:09:54 +0600

    Zod validate request and auth setup successful

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express'
+import ApiError from '../../errors/ApiError'
+import httpStatus from 'http-status'
+import { JwtHelper } from '../../helper/jwtHelper'
+import config from '../../config'
+import { Secret } from 'jsonwebtoken'
+
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Get authorization token
+      const token = req.headers.authorization
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized')
+      }
+      // verify token
+      let verifiedUser = null
+
+      verifiedUser = JwtHelper.verifyToken(token, config.jwt.secret as Secret)
+
+      req.user = verifiedUser
+      // Setup guard
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden')
+      }
+
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+export default auth
