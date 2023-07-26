632dba2343d3cfb72aa2374481176c7c9057cb7d
MajharolTanvir <majharul5456@gmail.com>
2023-07-26 09:50:05 +0600

    Uncaught rejection and uncaught expection setup successfully

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,45 @@ import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
 import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http'
+
+process.on('uncaughtException', err => {
+  errorLogger.error('Uncaught exception is detected...', err)
+  process.exit(1)
+})
+
+let server: Server
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
     logger.info(`🛢️ Database is connected successfully`)
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
     errorLogger.error(`Failed to connect database`, error)
   }
+
+  process.on('unhandledRejection', err => {
+    errorLogger.error('Unhandled rejection is detected...')
+    if (server) {
+      server.close(() => {
+        errorLogger.error(err)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
 main()
+
+// process.on('SIGTERM', () => {
+//   logger.info('SIGTERM is retrieved')
+//   if (server) {
+//     server.close()
+//   }
+// })
